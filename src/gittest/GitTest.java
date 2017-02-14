104b5495e4ff47761b3d36d63b74eb2378ab9dda
Søren Preus <spreus@gmail.com>
2017-02-14 14:48:44 +0100

    asdas
    askjdhaskjdhsakjd

---
+++ src/gittest/GitTest.java
@@ -0,0 +1,21 @@
+/*
+ * Copyrights protected by Noka Design Aps
+ * 
+ */
+package gittest;
+
+/**
+ *
+ * @author spreus
+ */
+public class GitTest {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("");
+        // TODO code application logic here
+    }
+
+}
